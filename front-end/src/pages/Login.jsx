ee7153b2fa867aa71c18f249685cc4231073d380
Leo <leonardoandrei41@gmail.com>
2022-10-31 19:07:37 -0300

    feat: elementos da tela de login

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,8 +1,25 @@
+import Button from '../components/button';
+import Input from '../components/input';
+
 export default function Login() {
   return (
     <div>
       <h1>Login</h1>
-      <p>Log in to your account</p>
+      <Input name="Login" dataTestId="common_login__input-email" type="email" />
+
+      <Input name="Senha" dataTestId="common_login__input-password" type="password" />
+
+      <Button
+        name="Log in"
+        type="submit"
+        dataTestId="common_login__button-login"
+      />
+
+      <Button
+        name="Ainda não tenho conta"
+        type="button"
+        dataTestId="common_login__button-register"
+      />
     </div>
   );
 }
