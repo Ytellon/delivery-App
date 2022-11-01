1fc8845488a8660111680a769cb2c02526c87d95
Leo <leonardoandrei41@gmail.com>
2022-11-01 13:42:05 -0300

    feat: endpoint /login - retorna token com login válido

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.use(express.json());
+
+app.use('/login', userRoutes);
 
 module.exports = app;
