293d836e0583cb25f04aba1de413455f142001f3
Leo <leonardoandrei41@gmail.com>
2022-11-01 16:42:54 -0300

    feat: /login retorna erro quando login é inválido

--- back-end/src/api/controllers/userController.js
+++ back-end/src/api/controllers/userController.js
@@ -1,14 +1,14 @@
 const userService = require('../services/userService');
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-
   try {
+    const { email, password } = req.body;
+
     const token = await userService.login({ email, password });
 
     res.status(200).json({ token });
   } catch (error) {
-    res.status(404).json({ message: 'User Not Found' });
+    res.status(error.code || 500).json({ message: error.message });
   }
 };
 
