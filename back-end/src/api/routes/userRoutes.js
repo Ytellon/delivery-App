ebf1e7371eab38a9974c6b8f955a21511c007b38
Leo <leonardoandrei41@gmail.com>
2022-11-04 17:29:19 -0300

    feat: endpoint GET /users

--- back-end/src/api/routes/userRoutes.js
+++ back-end/src/api/routes/userRoutes.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const userController = require('../controllers/userController');
 const validateLogin = require('../middlewares/validateLogin');
+const validateToken = require('../middlewares/validateToken');
+const adminAuth = require('../middlewares/adminAuth');
 
 const userRoutes = express.Router();
 
 userRoutes.post('/login', validateLogin, userController.login);
 
 userRoutes.post('/register', userController.createUser);
+
+userRoutes.get('/users', validateToken, adminAuth, userController.getAllUsers);
 
 module.exports = userRoutes;
